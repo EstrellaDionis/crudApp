740f7fffcf4abaf8bfa8908060b9401f1b52e3da
EstrellaDionis <EstrellaDionis@gmail.com>
2022-04-29 16:02:30 -0400

    wip

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "FullStackAppDataBase",
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello Dionis");
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'good movie')";
+
+  db.query(sqlInsert, (err, result) => {
+    res.send("Hello");
+  });
 });
 
 app.listen(3001, () => {
