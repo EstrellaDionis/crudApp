632f8d45a55e4a21a535545e70c43d0b6b5f2d73
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-01 18:21:25 -0400

    installed knex in server dir

--- server/data/migrations/20220501183712_table-movies.js
+++ server/data/migrations/20220501183712_table-movies.js
@@ -1,9 +1,11 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("movies", (table) => {
-    table.increments();
+    table.increments("movie_id");
+    table.text("movieName", 128).unique().notNullable();
+    table.integer("movieReview").notNullable();
   });
 };
 
 exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists("fruits");
+  await knex.schema.dropTableIfExists("movies");
 };
